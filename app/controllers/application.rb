e5b5c8e86ba06dfb8a3199478d89dea6de02951c
Christopher Grace <christopher.grace3@gmail.com>
2017-11-14 13:30:50 -0600

    finish episode 5

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,21 +1,21 @@
 
 # ***INDEX***
-# Display a list of all contacts
+# display a list of all contacts
 get '/contacts' do
   @contacts = Contact.all
   erb :'contacts/index'
 end
 
 # ***NEW***
-# Returns an HTML form for creating a new contact"
+# returns an HTML form for creating a new contact
 get '/contacts/new' do
+  @contact = Contact.new
   erb :'contacts/new'
 end
 
 # ***CREATE***
 # create a new contact
 post '/contacts' do
-  puts params
   @contact = Contact.create(params[:contact])
   redirect '/'
 end
@@ -29,13 +29,18 @@ get '/contacts/:id' do
 end
 
 # ***EDIT***
+# return an HTML form for editing a contact
 get '/contacts/:id/edit' do
-  "return an HTML form for editing a contact"
+  @contact = Contact.find(params[:id])
+  erb :'contacts/edit'
 end
 
 # ***UPDATE***
+# update a specific contact
 put '/contacts/:id' do
-  "update a specific contact"
+  @contact = Contact.find(params[:id])
+  @contact.update(params[:contact])
+  redirect "/contacts/#{@contact.id}"
 end
 
 # ***DELETE***
