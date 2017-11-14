ea1539e80ba405f728ada7147fe30dc909ff682a
Christopher Grace <christopher.grace3@gmail.com>
2017-11-14 13:46:16 -0600

    finish episode 6

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,20 +1,17 @@
 
 # ***INDEX***
-# display a list of all contacts
 get '/contacts' do
   @contacts = Contact.all
   erb :'contacts/index'
 end
 
 # ***NEW***
-# returns an HTML form for creating a new contact
 get '/contacts/new' do
   @contact = Contact.new
   erb :'contacts/new'
 end
 
 # ***CREATE***
-# create a new contact
 post '/contacts' do
   @contact = Contact.create(params[:contact])
   redirect '/'
@@ -22,21 +19,18 @@ end
 
 
 # ***SHOW***
-# show a specific contact
 get '/contacts/:id' do
   @contact = Contact.find(params[:id])
   erb :'contacts/show'
 end
 
 # ***EDIT***
-# return an HTML form for editing a contact
 get '/contacts/:id/edit' do
   @contact = Contact.find(params[:id])
   erb :'contacts/edit'
 end
 
 # ***UPDATE***
-# update a specific contact
 put '/contacts/:id' do
   @contact = Contact.find(params[:id])
   @contact.update(params[:contact])
@@ -45,5 +39,6 @@ end
 
 # ***DELETE***
 delete '/contacts/:id' do
-  "Delete a specific contact"
+  @contact = Contact.find(params[:id]).destroy!
+  redirect '/contacts'
 end
